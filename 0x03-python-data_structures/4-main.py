05b48e5d486793b9ca1979c437323ffb4ab2bea9
UmarMsani <umuhammad220@gmail.com>
2023-08-15 18:26:22 +0000

    function that replaces an element in a list

--- 0x03-python-data_structures/4-main.py
+++ 0x03-python-data_structures/4-main.py
@@ -1,4 +1,5 @@
-n_list = __import__('4-new_in_list').new_in_list
+#!/usr/bin/python3
+new_in_list = __import__('4-new_in_list').new_in_list
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
@@ -6,4 +7,4 @@ new_element = 9
 new_list = new_in_list(my_list, idx, new_element)
 
 print(new_list)
-print(my_list
+print(my_list)
