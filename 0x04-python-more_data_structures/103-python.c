09618d008a63bfde8e03ca65d0beb7e03b9981bd
UmarMsani <umuhammad220@gmail.com>
2023-08-20 09:59:06 +0000

    two C functions that print some basic info about Python lists and Python bytes objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -10,22 +10,25 @@
 
 void print_python_bytes(PyObject *p)
 {
-	if (!PyBytes_Check(p))
+	if (!p || !PyObject_IsInstance(p, (PyObject *)&PyBytes_Type))
 	{
 		printf("Not a Python bytes object.\n");
 		return;
 	}
 
+	Py_ssize_t bytes_size = ((PyVarObject *)p)->ob_size;
+	char *bytes_data = ((PyBytesObject *)p)->ob_sval;
+
 	printf("Python bytes info:\n");
 	printf("[.] bytes object info\n");
-	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
-	printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
+	printf("  size: %ld\n", bytes_size);
+	printf("  trying string: %s\n", bytes_data);
 
-	printf("  first %ld bytes: ", ((PyVarObject *)p)->ob_size);
-	for (Py_ssize_t i = 0; i < ((PyVarObject *)p)->ob_size; ++i)
+	printf("  first %ld bytes: ", bytes_size);
+	for (Py_ssize_t i = 0; i < bytes_size; ++i)
 	{
-		printf("%02x", ((PyBytesObject *)p)->ob_sval[i] & 0xFF);
-		if (i < ((PyVarObject *)p)->ob_size - 1)
+		printf("%02x", (unsigned char)bytes_data[i]);
+		if (i < bytes_size - 1)
 		{
 			printf(" ");
 		}
@@ -41,18 +44,19 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
-	if (!PyList_Check(p))
+	if (!p || !PyObject_IsInstance(p, (PyObject *)&PyList_Type))
 	{
 		printf("Not a Python list.\n");
 		return;
 	}
 
+	Py_ssize_t list_size = ((PyVarObject *)p)->ob_size;
 	printf("Python list info:\n");
-	printf("[*] Size of the list: %ld\n", PyList_Size(p));
+	printf("[*] Size of the list: %ld\n", list_size);
 
-	for (Py_ssize_t i = 0; i < PyList_Size(p); ++i)
+	for (Py_ssize_t i = 0; i < list_size; ++i)
 	{
-		PyObject *item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		PyObject *item = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, item->ob_type->tp_name);
 	}
 }
