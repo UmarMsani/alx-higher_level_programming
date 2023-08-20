36889000336046026e3aa27ce1cad8d988216e4d
UmarMsani <umuhammad220@gmail.com>
2023-08-20 07:44:27 +0000

    function that replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    '''
+        A function that traverse through a list for an element that matches
+        search and modify it with replace then returns a new list.
+        @elem: Elements
+    '''
     new_list = []
 
     for item in my_list:
