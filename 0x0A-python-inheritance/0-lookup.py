9fb1ac27c62a9d773da0cf260b1f24409f4856b2
UmarMsani <umuhammad220@gmail.com>
2023-09-13 13:43:56 +0000

    Python - Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this script defines below
+"""
+
+
+def lookup(obajana):
+    """
+    This is a function that returns list of available attributes
+    """
+
+    return dir(obajana)
