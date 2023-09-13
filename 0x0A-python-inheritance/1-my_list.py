8b555052704b8ef613d4ed80945ac18b3b065a99
UmarMsani <umuhammad220@gmail.com>
2023-09-13 14:11:28 +0000

    Python - Inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This Defines an subclass or child list class MyList."""
+
+
+class MyList(list):
+    """This class is subclass of the built-in list class."""
+
+    def print_sorted(mine):
+        """this prints sorted list in ascending order."""
+        print(sorted(mine))
