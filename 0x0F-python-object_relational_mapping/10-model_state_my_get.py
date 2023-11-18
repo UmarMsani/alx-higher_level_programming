9b56b948e50bf1cbbf2fd5bd6d65add6de0ec5fe
UmarMsani <umuhammad220@gmail.com>
2023-11-18 06:23:34 -0800

    update

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,7 +13,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} username password db_name state_name".format(sys.argv[0]))
+        print("Usage: {} \
+                username password db_name state_name".format(sys.argv[0]))
     else:
         username, password, db_name, state_name = \
                 sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
