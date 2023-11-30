7acc6c58b13ac055abde8aca3bd924b2f105be63
UmarMsani <umuhammad220@gmail.com>
2023-11-30 10:25:28 -0800

    Python - Network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to cause the server to respond with a message
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin:HolbertonSchool"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
